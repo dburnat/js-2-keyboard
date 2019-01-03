9538b385c2232d0cdc0d11fcdacdc8b3014be8f3
Dawid <Dawid@DESKTOP-7FKT8EH>
2019-01-03 21:32:18 +0100

    initial class for recording

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
-"use strict";
-
 let body = document.body;
 let record = document.querySelector('.record');
 let stop = document.querySelector('.stop');
 let soundClips = document.querySelector('.sound-clips');
 let canvas = document.querySelector('.visualizer');
 let mainSection = document.querySelector('.main-controls');
+let audioClip = document.querySelector('.clip');
 
 stop.disabled = true;
+
+class Record {
+    constructor(){
+        this.audioClip[10] = audioClip[10];
+        this.timeAt = timeAt;
+    }
+}
+
+let keyList = ['q' , 'w', 'e' , 'r' , 'a' , 's' , 'd' , 'f'];
+
+function startRecording(){
+    let record = new Record();
+    //if(body.addEventListener('keypress' , (e) == )
+}
+
+
+
+
+
 
 body.addEventListener('keypress' , (e)=>{
     switch(e.key){
